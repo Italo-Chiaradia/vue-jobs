f25270da2da20d1cb7155eead3f7dc73eab8d03e
Italo Chiaradia <102377060+Italo-Chiaradia@users.noreply.github.com>
2024-11-03 14:26:52 -0300

    final project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import Hero from '@/components/Hero.vue'
+  import HomeCards from '@/components/HomeCards.vue'
+  import JobsListening from '@/components/JobsListening.vue';
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <JobsListening :limit="3" :showButton="true" />
+  <section class="m-auto max-w-lg my-10 px-6">
+    <RouterLink
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</RouterLink
+    >
+  </section>
+</template>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,7 @@
+<script setup>
+  import JobsListening from '@/components/JobsListening.vue';
+</script>
+
+<template>
+  <JobsListening />
+</template>
